be13c7b885ecc0a67d78a9d748f23270fe07ecd1
Goekhan Tura <gtura@t-online.de>
2023-06-19 22:57:08 +0200

    kubernetes deployments

---
+++ k8s/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#minikube start --driver=qemu2 --container-runtime=cri-o
+#minikube ssh "sudo sysctl -w fs.inotify.max_user_watches=1048576"
+###############
+#funzt nicht ##
+###############
+#minikube start --extra-config='kubelet.allowed-unsafe-sysctls="fs.inotify.*"'
+
+kubectl apply -f configmap-edumeet.yaml
+kubectl apply -f deployment-redis.yaml
+kubectl apply -f deployment-edumeet.yaml
+kubectl apply -f deployment-pika.yamljour
